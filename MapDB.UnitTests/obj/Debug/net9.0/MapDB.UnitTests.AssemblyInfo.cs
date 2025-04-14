657d632bc3b9c0e2342005fed3beaf7838fb4913
woollyfleece <kiaoraskylar@gmail.com>
2025-04-14 16:47:25 +1200

    Functioning API. Created for MSwDev programme, uploaded for posterity.

--- MapDB.UnitTests/obj/Debug/net9.0/MapDB.UnitTests.AssemblyInfo.cs
+++ MapDB.UnitTests/obj/Debug/net9.0/MapDB.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapDB.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a525a174e65cb3eacbc66da9dbd307c527687714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee77ac9f351003c4253b973e061c18f30a825c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapDB.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapDB.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
